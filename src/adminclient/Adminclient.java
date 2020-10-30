b3f44327524dcdd5877c41c25db991da94faa3bb
Derek Gunnels <45681663+DGunnels@users.noreply.github.com>
2020-10-30 17:37:44 -0600

    Commit for GUI admin and user

---
+++ src/adminclient/Adminclient.java
@@ -0,0 +1,97 @@
+package adminclient;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+
+
+public class Adminclient {
+
+
+    public Tab addUser;
+    public Tab prodManagement;
+    public Tab catManagement;
+    public Tab ordReport;
+    public Button saveAndClose;
+    public TextField usernameField;
+    public PasswordField passwordField;
+    public ToggleButton adminToggle;
+    public ToggleGroup adminFlag;
+    public TextField prodnameField;
+    public Button updateCats;
+    public ComboBox prodCatSelBox;
+    public Button addProdClick;
+    public Button delProdClick;
+    public TextField catNameField;
+    public Button addCatClick;
+    public Button delCatClick;
+    public Button setDefaultCat;
+    public ChoiceBox defCategorySel;
+    public ListView userOrderViews;
+    public ListView userSelectOrderViews;
+    public DatePicker dateSelOrderView;
+    public ListView dateOrderView;
+    public Tab loginTab;
+    public TextField usernameFieldLogin;
+    public PasswordField passwordFieldLogin;
+    public Button loginButton;
+    public Button resetButton;
+
+    public void addUserClick(Event event) {
+    }
+
+    public void prodManClick(Event event) {
+    }
+
+    public void catManClick(Event event) {
+    }
+
+    public void ordReportClick(Event event) {
+    }
+
+    public void saveCloseClick(ActionEvent actionEvent) {
+    }
+
+    public void toggleAdmin(ActionEvent actionEvent) {
+    }
+
+    public void updatedCategories(ActionEvent actionEvent) {
+    }
+
+    public void selProdCategory(ActionEvent actionEvent) {
+    }
+
+    public void addProduct(ActionEvent actionEvent) {
+    }
+
+    public void deleteProduct(ActionEvent actionEvent) {
+    }
+
+    public void addCategory(ActionEvent actionEvent) {
+    }
+
+    public void deleteCategory(ActionEvent actionEvent) {
+    }
+
+    public void updateDefCat(ActionEvent actionEvent) {
+    }
+
+    public void selUserReportView(MouseEvent mouseEvent) {
+    }
+
+    public void dateUpdOrderView(ActionEvent actionEvent) {
+    }
+
+    public void ordReportClck(Event event) {
+    }
+
+    public void checkAuthLogin(Event event) {
+    }
+
+    public void loginUser(ActionEvent actionEvent) {
+    }
+
+    public void resetTextFieldsLogin(ActionEvent actionEvent) {
+    }
+}
